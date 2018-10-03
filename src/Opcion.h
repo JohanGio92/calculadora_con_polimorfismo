5ff76ebe65a107a24c4db91a785549c1c4d9fcac
Johan Revilla <johan.revilla.92@gmail.com>
2018-10-03 17:29:28 -0300

    defiendo la clase opcion

--- src/Opcion.h
+++ src/Opcion.h
@@ -1,9 +1,19 @@
 #ifndef OPCION_H_
 #define OPCION_H_
+#include <string>
+#include <iostream>
+
+typedef int Entero;
 
 class Opcion {
+private:
+	std::string titulo;
+	Entero unNumero;
+	Entero otroNumero;
 public:
 	Opcion();
+	virtual Entero ejecutar() = 0;
+
 	virtual ~Opcion();
 };
 
